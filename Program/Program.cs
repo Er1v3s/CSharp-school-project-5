7803ee50cfd2c9a68c4562d71b3ededb9fa2c2c1
Filip Statkiewicz <filip.statkiewicz25@gmail.com>
2022-12-05 22:56:12 +0100

    Create 2 classes, create loop to choose item

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Program;
+using System;
 
 namespace App
 {
@@ -6,7 +7,47 @@ namespace App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string userChose;
+            bool flag = false;
+
+            do
+            {
+                Console.WriteLine("Wybierz przedmiot, który Cię interesuje: ");
+                Console.WriteLine("1. Lodowka");
+                Console.WriteLine("2. Pralka \n");
+
+                userChose = Console.ReadLine();
+                userChose = userChose.ToLower();
+
+                Console.Clear();
+
+                if (userChose == "lodowka" || userChose == "lodówka" || userChose == "l" || userChose == "1")
+                {
+                    flag = true;
+                    Console.WriteLine("Wybrano Lodówkę \n");
+
+                    Fridge fridge = new Fridge();
+
+                }
+                else if (userChose == "pralka" || userChose == "p" || userChose == "2")
+                {
+                    flag = true;
+                    Console.WriteLine("Wybrano Pralke");
+
+                    WashingMachine washingMachine = new WashingMachine();
+                }
+                else
+                { 
+                    flag = false;
+                    Console.WriteLine("Nieprawidłowy wybór, spróbuj ponownie! \n");
+                }
+
+            } while(flag == false);
+
+
+
+            Console.ReadLine();
+
         }
     }
 }
